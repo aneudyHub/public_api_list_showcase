af18d569f0a57b454d315264db58cfa8f3afbcac
Aneudy Vargas <aneudyvargas4d23@gmail.com>
2023-08-31 12:36:43 -0400

    dependencies syncing and prepared for the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,39 @@
+val hiltVersion: String by rootProject.extra
+val lifecycleVersion: String by rootProject.extra
+val coroutinesVersion: String by rootProject.extra
+val retrofitVersion: String by rootProject.extra
+val httpLoggingVersion: String by rootProject.extra
+val gsonVersion: String by rootProject.extra
+val ktxCoreVersion: String by rootProject.extra
+val materialVersion: String by rootProject.extra
+val composeVersion: String by rootProject.extra
+val ktxExtensionVersion: String by rootProject.extra
+val mockitoVersion: String by rootProject.extra
+val jUnitVersion: String by rootProject.extra
+val navigationCompose: String by rootProject.extra
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
     namespace = "com.example.public_apis_list_showcase"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.public_apis_list_showcase"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
 
     buildTypes {
@@ -27,20 +46,60 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
     }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.core:core-ktx:$ktxCoreVersion")
+    implementation("com.google.android.material:material:$materialVersion")
+    implementation("androidx.activity:activity-compose:$composeVersion")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
+    implementation("androidx.navigation:navigation-compose:$navigationCompose")
+
+    //Hilt
+    implementation("com.google.dagger:hilt-android:$hiltVersion")
+    kapt("com.google.dagger:hilt-compiler:$hiltVersion")
+
+    //  Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
+
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+    implementation("com.squareup.okhttp3:logging-interceptor:$httpLoggingVersion")
+    implementation("com.google.code.gson:gson:$gsonVersion")
+
+    // kt
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-extensions:$ktxExtensionVersion")
+    kapt("androidx.lifecycle:lifecycle-compiler:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
+
+    // unit test
+    testImplementation("junit:junit:$jUnitVersion")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion")
+    testImplementation("org.mockito:mockito-core:$mockitoVersion")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,21 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+val kotlinVersion by extra { "1.9.10" }
+val retrofitVersion by extra { "2.6.0" }
+val httpLoggingVersion by extra { "3.12.0" }
+val gsonVersion by extra { "2.8.9" }
+val coroutinesVersion by extra { "1.5.0" }
+val hiltVersion by extra { "2.46.1" }
+val lifecycleVersion by extra { "2.5.1" }
+val ktxCoreVersion by extra { "1.10.1" }
+val composeVersion by extra { "1.7.2" }
+val materialVersion by extra { "1.9.0" }
+val ktxExtensionVersion by extra { "2.2.0" }
+val mockitoVersion by extra { "3.+" }
+val jUnitVersion by extra { "4.13.2" }
+val navigationCompose by extra { "2.7.1" }
+
 plugins {
     id("com.android.application") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.google.dagger.hilt.android") version "2.46.1" apply false
 }
